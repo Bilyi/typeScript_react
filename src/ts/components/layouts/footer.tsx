587bc1907e8d2a24305a0ee3204fa7bf5b36b06e
Bilyi <sergiy_biliy@ukr.net>
2020-10-24 19:00:20 +0300

    add responsive styles

--- src/ts/components/layouts/footer.tsx
+++ src/ts/components/layouts/footer.tsx
@@ -3,6 +3,15 @@ import * as React  from 'react';
 const Footer = () => {
     return (
         <div className="footer left-padding">
+            <ul className="footer-tabs">
+                <li>Home</li>
+                <li>Terms and Conditions</li>
+                <li>Privacy Policy</li>
+                <li>Collection Statement</li>
+                <li>Help</li>
+                <li>Manager Account</li>
+            </ul>
+            <p>Copyright &#169; 2020</p>
         </div>
     );
 };
